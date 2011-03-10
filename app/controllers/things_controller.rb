cde4957a05d91d9ab9034bcc12bdd58d19174435
Frankie Roberto <frankie@frankieroberto.com>
2011-03-10 21:27:47 +0000

    Bunch of other changes

--- app/controllers/things_controller.rb
+++ app/controllers/things_controller.rb
@@ -11,19 +11,25 @@ class ThingsController < ApplicationController
       
     else
 
-      @thing.update_attributes!(params[:thing])
-      #@thing.reload
+      thing_type_name = params[:thing][:type_of_thing_name].downcase
+
+      @thing.type_of_thing_name = thing_type_name
+      @thing.reload
       @things_count = @thing.type_of_thing.things.size
       if @things_count > 1
-        flash[:message] = "Thanks! There are now <strong><a href=\"#{type_of_thing_path(@thing.type_of_thing)}\">#{help.pluralize(@things_count, @thing.type_of_thing.name)}</a></strong> identified.".html_safe
+        flash[:message] = "Thanks! There are now <strong><a href=\"#{type_path(@thing.type_of_thing)}\">#{help.pluralize(@things_count, @thing.type_of_thing.name)}</a></strong> identified.".html_safe
         
       else  
-        flash[:message] = "Thanks! That's the first <strong><a href=\"#{type_of_thing_path(@thing.type_of_thing)}\">#{@thing.type_of_thing.name}</a></strong> identified.".html_safe
+        flash[:message] = "Thanks! That's the first <strong><a href=\"#{type_path(@thing.type_of_thing)}\">#{@thing.type_of_thing.name}</a></strong> identified.".html_safe
       end
     end
       
       redirect_to root_path
     
+  end
+  
+  def show
+    @thing = Thing.find(params[:id])
   end
 
 
