c1a7529445a641e387808fdb8dc0856dfc4485af
Frankie Roberto <frankie@frankieroberto.com>
2011-03-10 22:20:53 +0000

    Some styling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  before_filter :get_stats
+  
+  
+  def get_stats
+    @things_identified_count = Thing.count(:conditions => "type_of_thing_id IS NOT NULL")       
+    @thing_types = TypeOfThing.random_featured(5)
+  end  
+    
+
   def help
      Helper.instance
    end
--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -2,8 +2,6 @@ class HomepageController < ApplicationController
 
   def show    
     @thing = Thing.random_with_no_type 
-    @things_identified_count = Thing.count(:conditions => "type_of_thing_id IS NOT NULL")       
-    @thing_types = TypeOfThing.random_featured(5)
   end
   
 end
