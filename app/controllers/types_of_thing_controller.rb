12eea361f8156652de2513cc32e4488b15b61861
Frankie Roberto <frankie@frankieroberto.com>
2011-03-13 18:04:56 +0000

    Adding types index page

--- app/controllers/types_of_thing_controller.rb
+++ app/controllers/types_of_thing_controller.rb
@@ -1,5 +1,8 @@
 class TypesOfThingController < ApplicationController
 
+  def index
+    @types_of_thing = TypeOfThing.all(:order => :name)
+  end
 
   def show
     @type_of_thing = TypeOfThing.find(params[:id])
