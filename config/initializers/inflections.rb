397741a55d9a5a6c1afaa12c6b4296d0b1937ecb
Frankie Roberto <frankie@frankieroberto.com>
2011-03-15 09:53:54 +0000

    fix inflector

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -2,5 +2,5 @@
 
 ActiveSupport::Inflector.inflections do |inflect|
   inflect.irregular 'type_of_thing', 'types_of_thing'
-  inflect.irregular 'piece of skin', 'pieces of skin'
+  inflect.irregular 'piece of human skin', 'pieces of human skin'
 end
