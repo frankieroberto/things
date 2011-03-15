1898edf99e34ce5451c8785bf3bb38d92bebd6ff
Frankie Roberto <frankie@frankieroberto.com>
2011-03-15 09:46:38 +0000

    Link back to type page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,16 @@
 module ApplicationHelper
 
+  def a_or_an(name, include_name = true)
+    if name[0,1] =~ /[aeiou]/
+      article = "an"
+    else
+      article = "a"
+    end
+    if include_name
+      article + " " + name
+    else
+      article
+    end
+  end
 
 end
